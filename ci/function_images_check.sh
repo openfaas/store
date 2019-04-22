ffb7bb6bf690cc1efc5249fd640b30004fb1009e
Richard Gee <richard@technologee.co.uk>
2019-04-22 16:09:38 +0100

    Make ci/function_images_check.sh executable
    Signed-off-by: Richard Gee <richard@technologee.co.uk>

--- ci/function_images_check.sh
+++ ci/function_images_check.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running function image check..."
 
-images=($(cat functions.json | jq '.functions[].images[]' | sed 's/"//g;s/:/\/tags\//g'))
+images=($(cat functions.json | jq '.functions[].images[]'))
 
 for image in "${images[@]}"; do 
 
-    url="https://hub.docker.com/v2/repositories/${image}/"
+    stem=$(echo $image | sed 's/"//g;s/:/\/tags\//g')
+    url="https://hub.docker.com/v2/repositories/${stem}/"
     http_status=$(curl -Iks -X GET $url 2>/dev/null | head -n 1 | cut -d' ' -f2)
 
     if ! [ "$http_status" == "200" ]; then
-        echo "Failure: cURL of address $url returned a non-200 status code, exiting..."
+        echo "Failure: Check of ${image} returned a non-200 status code, exiting..."
         exit 1
     fi
 
