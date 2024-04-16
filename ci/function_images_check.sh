fd671c3530e0a0e533992d2a200cd2fc193dc62e
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-04-16 16:30:05 +0100

    Check each image
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- ci/function_images_check.sh
+++ ci/function_images_check.sh
@@ -4,17 +4,13 @@ echo "Running function image check..."
 
 images=($(cat functions.json | jq '.functions[].images[]'))
 
-for image in "${images[@]}"; do 
+uniq_images=($(echo "${images[@]}" |xargs -n1 | sort |uniq))
 
-    stem=$(echo $image | sed 's/"//g;s/:/\/tags\//g')
-    url="https://hub.docker.com/v2/repositories/${stem}/"
-    http_status=$(curl -Iks -X GET $url 2>/dev/null | head -n 1 | cut -d' ' -f2)
+for i in "${uniq_images[@]}"; do
+    image=$(echo $i | tr -d '"')
 
-    if ! [ "$http_status" == "200" ]; then
-        echo "Failure: Check of ${image} returned a non-200 status code, exiting..."
-        exit 1
-    fi
-
-done; 
+    echo "Checking image: ${image}"
+    crane manifest $image || { echo "Failure: Check of ${image} failed, exiting..."; exit 1; }
+done;
 
 echo "All function images were found"
